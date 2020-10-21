397d80ded6b5ab79156944793098c5a723df1c8b
HuangZe <ze.huang@changhong.com>
2020-10-21 17:37:53 +0800

    新建Environment类，用来存放Configuration.plist文件中的数据

---
+++ Environment/Configs/Environment.h
@@ -0,0 +1,16 @@
+//
+//  Environment.h
+//  Environment
+//
+//  Created by HuangZe on 2020/10/21.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface Environment : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Environment/Configs/Environment.m
@@ -0,0 +1,12 @@
+//
+//  Environment.m
+//  Environment
+//
+//  Created by HuangZe on 2020/10/21.
+//
+
+#import "Environment.h"
+
+@implementation Environment
+
+@end
