846a959c6ff5031c1bdb1cf9381b2089e207a68f
HuangZe <ze.huang@changhong.com>
2020-10-21 11:26:34 +0800

    创建按钮

--- Environment/ViewControllers/ViewController.m
+++ Environment/ViewControllers/ViewController.m
@@ -16,6 +16,13 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
+    btn.frame = CGRectMake(100, 100, 100, 50);
+    [btn setTitleColor:[UIColor blackColor] forState:UIControlStateNormal];
+    [btn setTitle:@"按钮" forState:UIControlStateNormal];
+    [btn setBackgroundColor:[UIColor redColor]];
+    [self.view addSubview:btn];
 }
 
 
